e5d04dac357d8486552930d781823361965694f0
Sam Dutton <samdutton@gmail.com>
2016-05-16 23:44:20 -0700

    Updated README and index.html

--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var nodeStatic = require('node-static');
-var http = require('http');
-var file = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(2014);
